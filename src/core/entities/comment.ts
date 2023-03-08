23edab24c46eca72f17b5bacac14524edd26613c
Borja Tur <borja.tur@gmail.com>
2023-03-08 19:29:32 +0100

    clean up entity file

--- src/core/entities/comment.ts
+++ src/core/entities/comment.ts
@@ -1,9 +1,3 @@
-// const commentSchema = new Schema({
-//   content: { type: String, required: true },
-//   authorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//   postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
-// });
-
 export interface Comment {
   id: string
   content: string
